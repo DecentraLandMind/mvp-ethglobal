37947848f914f01e9d0259887079e291bc8174a3
Arman <arman.payandeh.2025@gmail.com>
2025-07-30 20:01:16 +0330

    upgrade config.py

---
+++ llm-node/app/config.py
@@ -0,0 +1,7 @@
+MODEL_DIR = "/home/arman/Desktop/AI-BOX/mvp/llama-models/models"
+
+MODEL_FILE = "tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf"
+
+DOCKER_IMAGE = "llama-runner"
+
+EXECUTABLE = "./build/bin/llama-run"
